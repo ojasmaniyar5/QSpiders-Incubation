bd26468f5201ebb2ace9c6a2a99999b97486ac7d
Ojas Maniyar <arslan.m1506@gmail.com>
2025-02-24 10:46:13 +0530

    modified:   Python/QP_python_loops.py 	modified:   Python/QP_python_statement.py

--- Python/QP_python_loops.py
+++ Python/QP_python_loops.py
@@ -1,89 +1,67 @@
 # while Loop.
 
-# write a program to print numbers from 1 to 10 ?
+#1 write a program to print numbers from 1 to 10 ?
 
-# write a program to print reverse numbers from 1 to 10 ?
+#2 write a program to print reverse numbers from 1 to 10 ?
 
-# write a program to print n natural numbers ?
+#3 write a program to print n natural numbers ?
 
-# write a program to print all the even numbers between 1 to 100 ?
+#4 write a program to print all the even numbers between 1 to 100 ?
 
-# write a program to print the multiplication table ?
+#5 write a program to print the multiplication table ?
 
-# write a program to find sum of n natural numbers ?
+#6 write a program to find sum of n natural numbers ?
 
-# write a program to find factorial of n natural number ?
+#7 write a program to find factorial of n natural number ?
 
-# write a program to find all the string values present in the list ?
+#8 write a program to find all the string values present in the list ?
 
-# write a program to reverse a given integar without using typrcasting and slicing ?
+#9 write a program to reverse a given integar without using typrcasting and slicing ?
 
-# Write a program to print following output "Hey_How_Are_You" ?
+#10 Write a program to print following output "Hey_How_Are_You" ?
 
-# Write a program to extract all the special character to given string ?
+#11 Write a program to extract all the special character to given string ?
 
-# write a program to get a following output - ['a',97,'b',98,'c',99,'d',100] ?
+#12 write a program to get a following output - ['a',97,'b',98,'c',99,'d',100] ?
 
-# write a program to get a following output ?
+#13 write a program to get a following output ?
 # inp['hello',9j,8,90,'bye','bajaj']
 # output - ['hello':'ho','bye':'be','bajaj':'bj']
 
-# write a program to get a following output ?
+#14 write a program to get a following output ?
 # inp = 'HeLlo'
 # output = 'hElLO'
  
-# write a program to remove duplicate values from list without typecasting into set ?
+#15 write a program to remove duplicate values from list without typecasting into set ?
 
-# Write a  program to check wether the given number is armstrong or not ?
+#16 Write a  program to check wether the given number is armstrong or not ?
 
-# Write a program to count number of apprence of specified character without using in-built function ?
+#17 Write a program to count number of apprence of specified character without using in-built function ?
 
-# write a program to reverse a string without using slicing ?
+#18 write a program to reverse a string without using slicing ?
 
 #.................................................
 
 # For Loop.
 
-# Write a program to reverse a string without using slicing ? 
+#19 Write a program to reverse a string without using slicing ? 
 
-# Write a program to find the sum of all the float values present in a dict only if its key is of string datatype having len() more then 3 character ?
+#20 Write a program to find the sum of all the float values present in a dict only if its key is of string datatype having len() more then 3 character ?
 
-# WAP to find the sum of all the even integer present at even index at given list ?
+#21 WAP to find the sum of all the even integer present at even index at given list ?
 
-# WAP to print all the even number which are multiple of 4 and palindrome between range 1 to n ?
+#22 WAP to print all the even number which are multiple of 4 and palindrome between range 1 to n ?
 
-# WAP to find sum of n natural number only if it is even ?
+#23 WAP to find sum of n natural number only if it is even ?
 
-# WAP to excrect all the special character from the given string ?
+#24 WAP to excrect all the special character from the given string ?
 
-#WPA tp print the largest number in a list ?
+#25 WPA tp print the largest number in a list ?
 
-#WAP to exrect all the lower string only if ACII value is even ?
+#26 WAP to exrect all the lower string only if ACII value is even ?
 
-# WAPt to find the digit of 4 number ?
+#27 WAPt to find the digit of 4 number ?
 
-# WAP to count number of uppercase and lowercase character in a string ?
+#28 WAP to count number of uppercase and lowercase character in a string ?
 
-'''
-s = 'Hello World'
-uc = 0
-lc = 0
-
-for i in s:
-    if 'A' <= i <= 'Z':
-        uc += 1
-        if 'a' <= i <= 'z':
-            lc += 1
-
-print("Uppercase:",uc)
-print("Lowercase:",lc)
-'''
-
-# WAP to find a avrage of number from 1 to 10 ?
-'''
-sum = 9
-for i in range(1, 11):
-    sum += i
-    average = sum / 10
-print("Average:",average)
-'''
+#29 WAP to find a avrage of number from 1 to 10 ?
--- Python/QP_python_statement.py
+++ Python/QP_python_statement.py
@@ -1,84 +1,84 @@
 # if else.
 
-# Write a program to check the if the sum of 2 number is greater than 100
+#1 Write a program to check the if the sum of 2 number is greater than 100
 
-# Write a program if the first and last character of string is same ?
+#2 Write a program if the first and last character of string is same ?
 
-# Write a program to if the product of 2 number is even
+#3 Write a program to if the product of 2 number is even
 
-# Write a program in the given character is in uppercase or not ?
+#4 Write a program in the given character is in uppercase or not ?
 
-# Write a program if string contains space ?
+#5 Write a program if string contains space ?
 
-# Write a program to check if the number as 3 digits(take int value from user) 
+#6 Write a program to check if the number as 3 digits(take int value from user) 
 
-# Write a program to find greatest of two number ?
+#7 Write a program to find greatest of two number ?
 
-# write a program to print square of given int if it is even, print cube of the num if it odd ?
+#8 write a program to print square of given int if it is even, print cube of the num if it odd ?
 
-# write a program to check a special symbol which given by user ?
+#9 write a program to check a special symbol which given by user ?
 
-# consider a str input and print the concadinating sting of first and last char only if the length more then 5 ?
+#10 consider a str input and print the concadinating sting of first and last char only if the length more then 5 ?
 
-# consider two input whether both are pointing to both location.
+#11 consider two input whether both are pointing to both location.
 
-# consider a tuple and check wheather a tuple is homogenuos or hetrogeonus ?
+#12 consider a tuple and check wheather a tuple is homogenuos or hetrogeonus ?
 
-# write a program to check wether the given string is keyword or not ?
+#13 write a program to check wether the given string is keyword or not ?
 
-# Consider a list input print the value at given index if the length is even else print the value at odd index ?
+#14 Consider a list input print the value at given index if the length is even else print the value at odd index ?
 
-# write program to check if the given list is empty or not ?
+#15 write program to check if the given list is empty or not ?
 
-# write a program to check if the sum of first and last digit of a given number is even or odd?
+#16 write a program to check if the sum of first and last digit of a given number is even or odd?
 
-# write a program to check if the difference between two number is a multiple of 7 ?
+#17 write a program to check if the difference between two number is a multiple of 7 ?
 
-# write a program to check wether the given string is having middle character or not ? 
+#18 write a program to check wether the given string is having middle character or not ? 
 
-# write a program to check if two string are of equal length but not same ?
+#19 write a program to check if two string are of equal length but not same ?
 
 
 #...................................................
 
 #elseif.
 
-# write a program to check wether the enter character is in uppercase, lowercase, digit or in special character ?
+#20 write a program to check wether the enter character is in uppercase, lowercase, digit or in special character ?
 
-# write a program to check wether a integar is in single digit, doble digit, triple digit or more than three ?
+#21 write a program to check wether a integar is in single digit, doble digit, triple digit or more than three ?
 
-# Write a program to find geatest of three number ?
+#22 Write a program to find geatest of three number ?
 
-# Consider a character input, if it is in uppercase print the first digit of its skee value, if it is lower case than print the last digit od skee value, if it is digit print the remainder when is is divided by 3 else print the same character ?
+#23 Consider a character input, if it is in uppercase print the first digit of its skee value, if it is lower case than print the last digit od skee value, if it is digit print the remainder when is is divided by 3 else print the same character ?
 
-# write a program wether the given two points are in lines given qudrants ?
+#24 write a program wether the given two points are in lines given qudrants ?
 
-# Consider a character a input if it is in uppercase case convert it into lowercase, if it is in lowercase convert it into uppercase if it is in digit find squre of it and if it special character add 5 to ascc value and print value. print without using any function ?
+#25 Consider a character a input if it is in uppercase case convert it into lowercase, if it is in lowercase convert it into uppercase if it is in digit find squre of it and if it special character add 5 to ascc value and print value. print without using any function ?
 
-# write a program to check wether the number is divisible only by 5 and divisible only by 2 or both by 2 and 5 ?
+#26 write a program to check wether the number is divisible only by 5 and divisible only by 2 or both by 2 and 5 ?
 
-# Write a program to check if the given character is vowel consonent digit or special character ?
+#27 Write a program to check if the given character is vowel consonent digit or special character ?
 
-# Write a program to classified an angle based on its degree ?
+#28 Write a program to classified an angle based on its degree ?
 
 
 #..........................................................
 
 # nested if
 
-# write a program to print middle value of list only if it is string ?
+#29 write a program to print middle value of list only if it is string ?
 
-# Write a program to print first value of tuple only if it is string having length more than 5 and string should bee palindrone ?
+#30 Write a program to print first value of tuple only if it is string having length more than 5 and string should bee palindrone ?
 
-# Write a program to login in an application with valid username and valid password (enter the password only if username is valid) ?
+#31 Write a program to login in an application with valid username and valid password (enter the password only if username is valid) ?
 
-# Write a program to last value of string only if it palindrome string starting with vowels ?
+#32 Write a program to last value of string only if it palindrome string starting with vowels ?
 
-# Write a program to print reverse string only if it is strating with vowel ending with consonent and it should have middle value ?
+#33 Write a program to print reverse string only if it is strating with vowel ending with consonent and it should have middle value ?
 
-# Write a program to find greatest among three in numbers ?
+#34 Write a program to find greatest among three in numbers ?
 
-# write a program to find greatest among 4 numbers ?
+#35 write a program to find greatest among 4 numbers ?
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
